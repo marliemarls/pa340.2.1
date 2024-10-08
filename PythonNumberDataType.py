dda338c34a7582aa89c2faad39d8b323e4f20eba
Marlie Martinez <marlieahue@gmail.com>
2024-10-08 11:15:16 -0400

    first commit

---
+++ PythonNumberDataType.py
@@ -0,0 +1,44 @@
+#Question 1:
+num = 5
+num2 = 7
+print(num + num2)
+
+#Question 2:
+floatNum = float(10.5)
+floatNum2= float(1.1)
+print(floatNum + floatNum2)
+
+#Question 3:
+numInt= int(7)
+numFloat = float(4.5)
+print(numInt + numFloat)
+
+#Question 4: 
+intVar = int(12)
+intVar2 = int(8)
+sum = intVar2 / intVar
+print(sum)
+
+#Question 5: 
+floatVar = float(20.5)
+floatVar2 = float(4.5)
+res = floatVar / floatVar2
+print(res)
+
+#Question 6: 
+coffee = float(4.50)
+cappuccino = float(6.50)
+espresso = float(3.50)
+greenTea = float(2.50)
+print(coffee, cappuccino, espresso, greenTea)
+
+#Question 7: 
+SALES_TAX = 0.08
+subtotal = (coffee *3) + (cappuccino * 4) + (espresso * 2)
+totalSale = (subtotal * SALES_TAX) + subtotal
+print(totalSale)
+
+#Question 8: 
+from math import pi 
+area = pi * (63**2)
+print(area)
